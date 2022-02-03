1f521723776db0be8972fe1e5c20831bfa551456
dofes <91889957+dofes@users.noreply.github.com>
2022-02-04 03:40:14 +0800

    测试性内容-定时生成箱子

--- Template/Plugin.cpp
+++ Template/Plugin.cpp
@@ -13,7 +13,7 @@
 #include <MC/BlockActor.hpp>
 #include <random>
 Logger Tc("TreasureChest");
-
+int a = 1;
 //{"Findable":0b,"Items":[],"id":"Chest","isMovable":1b,"x":-328,"y":69,"z":-333}
 //{"Findable":1b,"Items":[],"LootTable":"loot_tables/chests/pillager_outpost.json","LootTableSeed":879397998,"id":"Chest","isMovable":1b,"x":-2218,"y":77,"z":425}
 void entry()
@@ -33,4 +33,29 @@ void entry()
 		blac->setNbt(nbt->asCompoundTag());
 		return true;
 		});
+}
+
+THook(void, "?tick@ServerLevel@@UEAAXXZ",
+	void* _this)
+{
+	a++;
+	Tc.info("{}",a);
+	if (Level::getAllPlayers().size() != 0 && a%200 == 0)
+	{
+		for (Player *pl:Level::getAllPlayers())
+		{
+			BlockPos pos = pl->getPosition();
+			Level::setBlock(pos, 0, "minecraft:chest", 54);
+			BlockSource* bs = Level::getBlockSource(0);
+			BlockActor* blac = Level::getBlockEntity(pos, bs);
+			auto nbt = blac->getNbt();
+			{
+				std::default_random_engine e(time(0));
+				nbt->putString("LootTable", "loot_tables/chests/pillager_outpost.json");
+				nbt->putInt("LootTableSeed", e());
+			}
+			blac->setNbt(nbt->asCompoundTag());
+		}
+	}
+	return original(_this);
 }
